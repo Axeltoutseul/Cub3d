0cd878fdaca79f80449e645046a3e7f073f54aed
Axel Baudrillart <axbaudri@f2r2s9.clusters.42paris.fr>
2025-05-29 18:47:50 +0200

    cpp02

--- cub3d.h
+++ cub3d.h
@@ -1,9 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axbaudri <axbaudri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 18:23:18 by axbaudri          #+#    #+#             */
+/*   Updated: 2025/05/29 18:42:57 by axbaudri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
-#define CUB3D_H
-#include "libft/libft.h"
-#include "mlx_linux/mlx.h"
-#include "mlx_linux/mlx_int.h"
-
-
+# define CUB3D_H
+# include "libft/libft.h"
+# include "mlx_linux/mlx.h"
+# include "mlx_linux/mlx_int.h"
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axbaudri <axbaudri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 18:23:12 by axbaudri          #+#    #+#             */
+/*   Updated: 2025/05/29 18:23:59 by axbaudri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-int main(void)
+int	main(void)
 {
-    void    *mlx;
-    void    *mlx_win;
+	void	*mlx;
+	void	*mlx_win;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Bonjour");
-    return (0);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Bonjour");
+	return (0);
 }
